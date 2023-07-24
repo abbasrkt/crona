a5413b3f4cfa559985e82e9b919648770ee44071
عباس روحبخش <abbasrkt@gmail.com>
2023-07-24 14:56:14 +0330

    commit3
    commit3

--- modelvgg.py
+++ modelvgg.py
@@ -1,5 +1,5 @@
 from tensorflow.python.keras.models import load_model
-#test2
+#test3
 
 model = load_model('D:\cropImages - Covid\weights.00100-vl0.00.h5')
 model.load_weights('D:\cropImages - Covid\weights.00100-vl0.00.h5')
