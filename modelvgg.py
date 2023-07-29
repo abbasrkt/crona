d60031244389237679d48cdddeabebfb124b07c8
عباس روحبخش <abbasrkt@gmail.com>
2023-07-29 16:34:00 +0330

    test4
    descr

--- modelvgg.py
+++ modelvgg.py
@@ -1,5 +1,5 @@
 from tensorflow.python.keras.models import load_model
-#test3
+#test4
 
 model = load_model('D:\cropImages - Covid\weights.00100-vl0.00.h5')
 model.load_weights('D:\cropImages - Covid\weights.00100-vl0.00.h5')
